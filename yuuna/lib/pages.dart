e5ca35a03428ce857b546c2364b98613dac62974
lrorpilla <leorafaelorpilla@gmail.com>
2023-08-04 00:49:14 +1000

    Structured content support via dynamic height WebView

--- yuuna/lib/pages.dart
+++ yuuna/lib/pages.dart
@@ -12,6 +12,7 @@ export 'src/pages/implementations/dictionary_history_page.dart';
 export 'src/pages/implementations/dictionary_dialog_page.dart';
 export 'src/pages/implementations/dictionary_dialog_delete_page.dart';
 export 'src/pages/implementations/dictionary_dialog_import_page.dart';
+export 'src/pages/implementations/dictionary_structured_content_page.dart';
 export 'src/pages/implementations/language_dialog_page.dart';
 export 'src/pages/implementations/massif_sentences_dialog_page.dart';
 export 'src/pages/implementations/media_source_picker_dialog_page.dart';
