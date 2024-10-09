43d2d05897d9987b76e6d735e730bc2201e99f4b
Arianne Orpilla <arianneorpilla@protonmail.com>
2024-10-09 18:41:18 +1000

    Fix lint

--- yuuna/lib/src/pages/implementations/audio_recorder_page.dart
+++ yuuna/lib/src/pages/implementations/audio_recorder_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:multi_value_listenable_builder/multi_value_listenable_builder.dart';
+// ignore: depend_on_referenced_packages
 import 'package:record_mp3_plus/record_mp3_plus.dart';
 import 'package:spaces/spaces.dart';
 import 'package:yuuna/i18n/strings.g.dart';
